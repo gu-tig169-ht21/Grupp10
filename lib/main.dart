c1936304acd921e5058de193a8abca2be7900562
Agnes <agnesaronssonfasth@hotmail.com>
2021-12-10 10:23:27 +0100

    firstui

--- lib/main.dart
+++ lib/main.dart
@@ -3,35 +3,18 @@ import 'views/home.dart';
 
 void main() {
   runApp(const MyApp());
-  // hej :)
-  // hejhej :()
-  // kai hejsan
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(canvasColor: Color(0xff464646), fontFamily: 'Helvetica'),
+      title: 'Nicotine Tracker',
+      home: MyHomePage(),
     );
   }
 }
-
-// hej hej
-
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,86 +1,129 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  const MyHomePage({Key? key}) : super(key: key);
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int selectedIndex = 0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+        length: 2,
+        //  initialIndex:
+        child: Scaffold(
+            appBar: AppBar(
+                backgroundColor: const Color(0xff464646),
+                title: const Text('Nicotine Tracker',
+                    style: TextStyle(color: Colors.white)),
+                leading: PopupMenuButton(
+                    color: const Color(0xff464646),
+                    icon: const Icon(Icons.menu, color: Colors.white),
+                    onSelected: (value) {},
+                    itemBuilder: (context) => [
+                          const PopupMenuItem(
+                              child: Text(
+                                'Välj standardsnus',
+                                textAlign: TextAlign.right,
+                              ),
+                              textStyle: TextStyle(color: Colors.white),
+                              value: 0),
+                          const PopupMenuItem(
+                              child: Text('Inställningar'),
+                              textStyle: TextStyle(color: Colors.white),
+                              value: 1),
+                          const PopupMenuItem(
+                              child: Text('Integritet'),
+                              textStyle: TextStyle(color: Colors.white),
+                              value: 2),
+                        ]),
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(text: 'Konsumtion'),
+                    Tab(text: 'Ekonomi'),
+                  ],
+                  indicatorColor: Color(0xff95C8A8),
+                )),
+            bottomNavigationBar: BottomNavigationBar(
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle),
+                  label: 'Konsumtion',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.access_time),
+                  label: 'Historik',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.trending_up),
+                  label: 'Prognos',
+                ),
+              ],
+              backgroundColor: const Color(0xff464646),
+              unselectedItemColor: Colors.white,
+              selectedItemColor: const Color(0xff95C8A8),
+              currentIndex: selectedIndex,
+              onTap: (index) => setState(() {
+                selectedIndex = index;
+              }),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _timer(),
+                  Container(height: 80),
+                  _konsumtion(),
+                ],
+              ),
             ),
-          ],
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: const Color(0xff95C8A8),
+              onPressed: _incrementCounter,
+              tooltip: 'Lägg till prilla',
+              child: const Icon(Icons.add),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.endFloat));
+  }
+
+  Widget _timer() {
+    return Column(children: const [
+      Text('Du tog din senaste prilla för',
+          style: TextStyle(color: Colors.white)),
+      Text(
+        '0',
+        style: TextStyle(
+          color: Color(0xff95C8A8),
+          fontSize: 70.0,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      Text('minuter sedan', style: TextStyle(color: Colors.white)),
+    ]);
+  }
+
+  Widget _konsumtion() {
+    return Column(children: [
+      const Text('Du har totalt konsumerat',
+          style: TextStyle(color: Colors.white)),
+      Text(
+        '$_counter',
+        style: const TextStyle(
+          color: Color(0xff95C8A8),
+          fontSize: 70.0,
+        ),
+      ),
+      const Text('prillor idag', style: TextStyle(color: Colors.white)),
+    ]);
   }
 }
