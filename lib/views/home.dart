5ecc8ddf34ddc86dd6b7eaa5abf26eebb36ecb6c
cenkvist <Brothars1@gmail.com>
2021-12-13 10:37:26 +0100

    fixat tabs

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -15,6 +15,9 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int selectedIndex = 0;
 
+  int _costTotalCounter = 25;
+  int _costTodayCounter = 0;
+
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -24,89 +27,113 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        //  initialIndex:
-        child: Scaffold(
-            appBar: AppBar(
-                backgroundColor: const Color(0xff464646),
-                title: const Text('Nicotine Tracker',
-                    style: TextStyle(color: Colors.white)),
-                leading: PopupMenuButton(
-                    color: const Color(0xff464646),
-                    icon: const Icon(Icons.menu, color: Colors.white),
-                    onSelected: (value) {},
-                    itemBuilder: (context) => [
-                          const PopupMenuItem(
-                              child: Text(
-                                'Välj standardsnus',
-                                textAlign: TextAlign.right,
-                              ),
-                              textStyle: TextStyle(color: Colors.white),
-                              value: 0),
-                          const PopupMenuItem(
-                              child: Text('Inställningar'),
-                              textStyle: TextStyle(color: Colors.white),
-                              value: 1),
-                          const PopupMenuItem(
-                              child: Text('Integritet'),
-                              textStyle: TextStyle(color: Colors.white),
-                              value: 2),
-                        ]),
-                bottom: const TabBar(
-                  tabs: [
-                    Tab(text: 'Konsumtion'),
-                    Tab(text: 'Ekonomi'),
-                  ],
-                  indicatorColor: Colors.transparent,
-                  labelStyle: TextStyle(fontSize: 17, fontFamily: 'helvetica'),
-                  unselectedLabelStyle: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 15,
-                      fontFamily: 'helvetica'),
-                )),
-            bottomNavigationBar: BottomNavigationBar(
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Hem',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.history),
-                  label: 'Historik',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.timeline),
-                  label: 'Prognos',
-                ),
-              ],
-              backgroundColor: const Color(0xff464646),
-              unselectedItemColor: Colors.grey,
-              selectedItemColor: Colors.white,
-              currentIndex: selectedIndex,
-              onTap: (index) => setState(() {
-                selectedIndex = index;
-              }),
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _timer(),
-                  Container(height: 80),
-                  _konsumtion(),
-                ],
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color(0xff464646),
+          title: const Text('Nicotine Tracker', style: TextStyle(color: Colors.white)),
+          leading: PopupMenuButton(
+            color: const Color(0xff464646),
+            icon: const Icon(Icons.menu, color: Colors.white),
+            onSelected: (value) {},
+            itemBuilder: (context) => [
+                  const PopupMenuItem(
+                      child: Text(
+                        'Välj standardsnus',
+                        textAlign: TextAlign.right,
+                      ),
+                      textStyle: TextStyle(color: Colors.white),
+                      value: 0),
+                  const PopupMenuItem(
+                      child: Text('Inställningar'),
+                      textStyle: TextStyle(color: Colors.white),
+                      value: 1),
+                  const PopupMenuItem(
+                      child: Text('Integritet'),
+                      textStyle: TextStyle(color: Colors.white),
+                      value: 2),
+                ]),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(text: 'Konsumtion'),
+              Tab(text: 'Ekonomi'),
+            ],
+            indicatorColor: Color(0xff95C8A8),
+            labelStyle: TextStyle(
+              fontSize: 17,
+              fontFamily: 'helvetica',
               ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              backgroundColor: const Color(0xff95C8A8),
-              onPressed: _incrementCounter,
-              tooltip: 'Lägg till prilla',
-              child: const Icon(Icons.add),
-            ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.endFloat));
+            unselectedLabelStyle: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 15,
+              fontFamily: 'helvetica'),
+          ),
+              ),
+          bottomNavigationBar: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Hem',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.history),
+                label: 'Historik',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.timeline),
+                label: 'Prognos',
+              ),
+            ],
+            backgroundColor: const Color(0xff464646),
+            unselectedItemColor: Colors.grey,
+            selectedItemColor: Colors.white,
+            currentIndex: selectedIndex,
+            onTap: (index) => setState(() {
+              selectedIndex = index;
+            }),
+          ),
+          body: TabBarView(
+            children: [
+              Scaffold(
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _timer(),
+                      Container(height: 80),
+                      _konsumtion(),
+                    ],
+                  ),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  backgroundColor: const Color(0xff95C8A8),
+                  onPressed: _incrementCounter,
+                  tooltip: 'Lägg till prilla',
+                  child: const Icon(Icons.add),
+                ),
+                floatingActionButtonLocation:
+                    FloatingActionButtonLocation.endFloat
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _kostnadTotalt(),
+                    Container(height: 80),
+                    _kostnadIdag(),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+
+      ),
+    );
   }
 
   Widget _timer() {
@@ -138,4 +165,51 @@ class _MyHomePageState extends State<MyHomePage> {
       const Text('prillor idag', style: TextStyle(color: Colors.white)),
     ]);
   }
+
+  Widget _kostnadTotalt() {
+    return Column(
+      children: [
+        const Text('Pengar du spenderat på snus sedan start', style: TextStyle(color: Colors.white)),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$_costTotalCounter',
+              style: const TextStyle(
+                color: Color(0xff95C8A8),
+                fontSize: 70.0,
+              ),
+            ),
+            const SizedBox(width: 5),
+            const Text('kr', style: TextStyle(color: Colors.white)),        
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _kostnadIdag() {
+    return Column(
+      children: [        
+        const Text('Pengar du spenderat på snus idag', style: TextStyle(color: Colors.white)),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,          
+          children: [
+            Text(
+              '$_costTodayCounter',
+              style: const TextStyle(
+                color: Color(0xff95C8A8),
+                fontSize: 70.0,
+              ),
+            ),
+            const SizedBox(width: 5),            
+            const Text('kr', style: TextStyle(color: Colors.white)),
+          ],
+        ),
+      ],
+    );    
+  }
+
 }
