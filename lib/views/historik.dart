11bcc465302c898bc3705946d4b592c1580cfd36
Agnes <agnesaronssonfasth@hotmail.com>
2021-12-16 11:17:39 +0100

    historik

--- lib/views/historik.dart
+++ lib/views/historik.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyHistorikPage extends StatefulWidget {
   const MyHistorikPage({Key? key}) : super(key: key);
@@ -12,7 +13,136 @@ class _MyHistorikPageState extends State<MyHistorikPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Text('historik'),
-    ));
+            child: ListView(
+      padding: EdgeInsets.all(10),
+      children: [
+        _graf(),
+        // Container(height: 15),
+        Row(
+          children: [
+            Text('SENASTE VECKAN',
+                style: GoogleFonts.roboto(fontSize: 12, color: Colors.grey)),
+          ],
+        ),
+        _lastWeek(),
+        Container(height: 10),
+        Row(
+          children: [
+            Text('SENASTE MÅNADEN',
+                style: GoogleFonts.roboto(fontSize: 12, color: Colors.grey)),
+          ],
+        ),
+        _lastMonth(),
+        Container(height: 10),
+        Row(
+          children: [
+            Text('SENASTE ÅRET',
+                style: GoogleFonts.roboto(fontSize: 12, color: Colors.grey)),
+          ],
+        ),
+        _lastYear(),
+      ],
+    )));
   }
+
+  Widget _graf() {
+    return Container(
+        padding: const EdgeInsets.all(10),
+        child: Align(
+            alignment: Alignment.center,
+            child: Container(
+                width: 250.0,
+                height: 150.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: const Color(0xff282828),
+                ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('(graf eller kalender)',
+                          style: TextStyle(fontSize: 18, color: Colors.grey))
+                    ]))));
+  }
+}
+
+Widget _lastWeek() {
+  return Container(
+      padding: const EdgeInsets.fromLTRB(60, 30, 60, 20),
+      width: double.infinity,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: const Color(0xff282828),
+      ),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <
+          Widget>[
+        Column(children: const [
+          Text('120', style: TextStyle(fontSize: 28, color: Color(0xff699985))),
+          Text('kr', style: TextStyle(fontSize: 12, color: Colors.white)),
+        ]),
+        Column(children: const [
+          Text('3', style: TextStyle(fontSize: 28, color: Color(0xff699985))),
+          Text('dosor', style: TextStyle(fontSize: 12, color: Colors.white)),
+        ]),
+        Column(children: const [
+          Text('60', style: TextStyle(fontSize: 28, color: Color(0xff699985))),
+          Text('prillor', style: TextStyle(fontSize: 12, color: Colors.white)),
+        ])
+      ]));
+}
+
+_lastMonth() {
+  return Container(
+      padding: const EdgeInsets.fromLTRB(60, 30, 60, 20),
+      width: double.infinity,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: const Color(0xff282828),
+      ),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <
+          Widget>[
+        Column(children: const [
+          Text('670', style: TextStyle(fontSize: 28, color: Color(0xff699985))),
+          Text('kr', style: TextStyle(fontSize: 12, color: Colors.white)),
+        ]),
+        Column(children: const [
+          Text('12', style: TextStyle(fontSize: 28, color: Color(0xff699985))),
+          Text('dosor', style: TextStyle(fontSize: 12, color: Colors.white)),
+        ]),
+        Column(children: const [
+          Text('250', style: TextStyle(fontSize: 28, color: Color(0xff699985))),
+          Text('prillor', style: TextStyle(fontSize: 12, color: Colors.white)),
+        ])
+      ]));
+}
+
+_lastYear() {
+  return Container(
+      padding: const EdgeInsets.fromLTRB(60, 30, 60, 20),
+      width: double.infinity,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: const Color(0xff282828),
+      ),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <
+          Widget>[
+        Column(children: const [
+          Text('8000',
+              style: TextStyle(fontSize: 28, color: Color(0xff699985))),
+          Text('kr', style: TextStyle(fontSize: 12, color: Colors.white)),
+        ]),
+        Column(children: const [
+          Text('150', style: TextStyle(fontSize: 28, color: Color(0xff699985))),
+          Text('dosor', style: TextStyle(fontSize: 12, color: Colors.white)),
+        ]),
+        Column(children: const [
+          Text('3000',
+              style: TextStyle(fontSize: 28, color: Color(0xff699985))),
+          Text('prillor', style: TextStyle(fontSize: 12, color: Colors.white)),
+        ])
+      ]));
 }
