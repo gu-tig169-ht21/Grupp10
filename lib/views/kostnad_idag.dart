13badcb89a8000cd569a2a3782c50d48818fc61d
Kai Rowley <zerenity@gmail.com>
2021-12-16 11:20:40 +0100

    klasswidgets reformatering

---
+++ lib/views/kostnad_idag.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class KostnadIdag extends StatelessWidget {
+  KostnadIdag(this._costTodayCounter);
+
+  int _costTodayCounter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text('Pengar du spenderat på snus idag',
+            style: TextStyle(color: Colors.white)),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$_costTodayCounter',
+              style: const TextStyle(
+                color: Color(0xff95C8A8),
+                fontSize: 70.0,
+              ),
+            ),
+            const SizedBox(width: 5),
+            const Text('kr', style: TextStyle(color: Colors.white)),
+          ],
+        ),
+      ],
+    );
+  }
+}
